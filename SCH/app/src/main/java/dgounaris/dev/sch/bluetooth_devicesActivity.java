dedbf94b2a130a9dbb28449fc9c0bc10c9403009
AntonisKl <denti20082008@hotmail.com>
2017-08-03 21:13:09 +0300

    Added Bluetooth discovery for nearby devices
    Devices that are discovered are shown in a list

--- SCH/app/src/main/java/dgounaris/dev/sch/bluetooth_devicesActivity.java
+++ SCH/app/src/main/java/dgounaris/dev/sch/bluetooth_devicesActivity.java
@@ -1,104 +1,100 @@
 package dgounaris.dev.sch;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 import dgounaris.dev.sch.adapter.Bluetooth_devicesAdapter;
 
-import static android.R.attr.data;
-
 public class bluetooth_devicesActivity extends AppCompatActivity {
+
 
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_devices);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-        }
-
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-
-            startActivityForResult(enableBtIntent, 0);
+            startActivityForResult(enableBtIntent, 1);
         } else {
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            if (mBluetoothAdapter.isDiscovering())
+                mBluetoothAdapter.cancelDiscovery();
 
+            // check for permissions
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
 
-            ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, filter);
+            mBluetoothAdapter.startDiscovery();
 
+            // HERE WILL BE ADDED ON CLICK LISTENER FOR LIST ITEM IN ORDER TO RETURN TO CONNECTED_FRAGMENT
+        }
+    }
 
-            if (pairedDevices.size() > 0) {
-                for (BluetoothDevice device : pairedDevices) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-                    devices.add(device);
-                }
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "BlueTooth is now Enabled", Toast.LENGTH_LONG).show();
 
-                Bluetooth_devicesAdapter bluetoothDeviceArrayAdapter = new Bluetooth_devicesAdapter(this, devices);
+                if (mBluetoothAdapter.isDiscovering())
+                    mBluetoothAdapter.cancelDiscovery();
 
-                ListView listView = (ListView) findViewById(R.id.bluetooth_list);
-                listView.setAdapter(bluetoothDeviceArrayAdapter);
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(mReceiver, filter);
+                mBluetoothAdapter.startDiscovery();
 
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        finish(); // RETURN TO CONNECTED_FRAGMENT
-                    }
-                }, 3000);
                 // HERE WILL BE ADDED ON CLICK LISTENER FOR LIST ITEM IN ORDER TO RETURN TO CONNECTED_FRAGMENT
             }
         }
     }
 
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                ListView listView = (ListView) findViewById(R.id.bluetooth_list);
+                ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+                Bluetooth_devicesAdapter bluetoothDeviceArrayAdapter = new Bluetooth_devicesAdapter(context, devices);
+                devices.add(device);
+                listView.setAdapter(bluetoothDeviceArrayAdapter);
+            } else
+                Toast.makeText(getApplicationContext(), "No device found", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-// TODO Auto-generated method stub
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(getApplicationContext(), "BlueTooth is now Enabled", Toast.LENGTH_LONG).show();
-
-                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-
-                ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
-
-
-                if (pairedDevices.size() > 0) {
-                    for (BluetoothDevice device : pairedDevices) {
-
-                        devices.add(device);
-                    }
-
-                    Bluetooth_devicesAdapter bluetoothDeviceArrayAdapter = new Bluetooth_devicesAdapter(this, devices);
-
-                    ListView listView = (ListView) findViewById(R.id.bluetooth_list);
-                    listView.setAdapter(bluetoothDeviceArrayAdapter);
-
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            finish(); // RETURN TO CONNECTED_FRAGMENT
-                        }
-                    }, 3000);
-                    // HERE WILL BE ADDED ON CLICK LISTENER FOR LIST ITEM IN ORDER TO RETURN TO CONNECTED_FRAGMENT
-                }
-            }
-        }
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
+
