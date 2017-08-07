1449386742b78cbb4d0ff7783fe2d5facd4e9cb3
AntonisKl <denti20082008@hotmail.com>
2017-08-07 11:57:54 +0300

    Added some files for bluetooth connection

---
+++ SCH/app/src/main/java/dgounaris/dev/sch/BluetoothConnection/BluetoothConnectThread.java
@@ -0,0 +1,52 @@
+package dgounaris.dev.sch.BluetoothConnection;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by antonis on 04-Aug-17.
+ */
+
+public class BluetoothConnectThread extends Thread {
+    private BluetoothSocket bTSocket;
+
+    public boolean connect(BluetoothDevice bTDevice, UUID mUUID) {
+
+        try {
+            bTSocket = bTDevice.createRfcommSocketToServiceRecord(mUUID);
+        } catch (IOException e) {
+            Log.d("CONNECTTHREAD", "Could not create RFCOMM socket:" + e.toString());
+            return false;
+        }
+        try {
+            bTSocket.connect();
+        } catch (IOException e) {
+            Log.d("CONNECTTHREAD", "Could not connect: " + e.toString());
+            try {
+                bTSocket.close();
+            } catch (IOException close) {
+                Log.d("CONNECTTHREAD", "Could not close connection:" + e.toString());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean cancel() {
+        try {
+            bTSocket.close();
+        } catch (IOException e) {
+            Log.d("CONNECTTHREAD", "Could not close connection:" + e.toString());
+            return false;
+        }
+        return true;
+    }
+
+    public BluetoothSocket getbTSocket() {
+        return bTSocket;
+    }
+}
